fe5ad5428da8398979676eb6736553b26b5925fb
Vicky L <vicky.sy.loo@gmail.com>
2016-03-23 17:18:20 -0400

    exercise 7 completed

--- exercise7.rb
+++ exercise7.rb
@@ -1,9 +1,8 @@
-def student_data(year)
-  students.each do |cohort, number|
-  puts "#{cohort}: #{number} students"
+def display_classes (group)
+  group.each do |cohort, number|
+    puts "#{cohort}: #{number} students"
   end
 end
-
 
 students = {
   :cohort1 => 34,
@@ -11,5 +10,16 @@ students = {
   :cohort3 => 22
 }
 
+display_classes(students)
 
 students[:cohort4] = "43"
+
+puts students.keys
+
+students.each do |cohort, number|
+  puts "#{number * 1.5}"
+end
+
+students.delete(:cohort2)
+
+display_classes(students)
