7b8dc21bbc3de887691d35358a7c62c77594f0fd
Vicky L <vicky.sy.loo@gmail.com>
2016-03-23 14:28:25 -0400

    Exercise 5 complete, remember to add .to_i to end of gets.chomp, saves time

---
+++ exercise5.rb
@@ -0,0 +1,9 @@
+puts "What is your preferred room temperature (in Fahrenheit)?"
+
+fahrenheit = gets.chomp.to_i
+
+def temperature(num)
+  new_temperature = ( num - 32 ) * 5/9
+end
+
+puts "Your temperature is #{temperature(fahrenheit)} in Celsius!"
