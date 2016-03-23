e621d446decb84ce0b25dde7e578e05035c21957
Vicky L <vicky.sy.loo@gmail.com>
2016-03-23 14:55:24 -0400

    Realized I forgot to do .each for asterisk list, have now corrected

--- exercise6.rb
+++ exercise6.rb
@@ -1,1 +1,24 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+grocery_list.each do |produce|
+  puts "* #{produce}"
+end
+
+grocery_list << "rice"
+# => ["carrots", "toilet paper", "apples", "salmon", "rice"]
+
+grocery_list
+
+grocery_list.length # => 5
+
+if grocery_list.include?("bananas")
+  puts "You need to pick up bananas."
+else
+  puts "You don't need to pick up bananas today."
+end
+
+# => You don't need to pick up bananas today
+
+grocery_list[1] # => toilet paper
+
+grocery_list.sort("*")
