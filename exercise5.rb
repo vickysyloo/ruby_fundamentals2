2715ec4564bf7c369bf94f8e70892cc2bc27c176
Vicky L <vicky.sy.loo@gmail.com>
2016-03-23 21:53:15 -0400

    added round command in exercise 5

--- exercise5.rb
+++ exercise5.rb
@@ -4,6 +4,7 @@ fahrenheit = gets.chomp.to_i
 
 def temperature(num)
   new_temperature = ( num - 32 ) * 5/9.0
+  new_temperature.round(2)
 end
 
 puts "Your temperature is #{temperature(fahrenheit)} in Celsius!"
