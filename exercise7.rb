f5cdf92c3900752aa0863ddce04d2b730c501087
Vicky L <vicky.sy.loo@gmail.com>
2016-03-23 21:19:23 -0400

    final editing of exercise 7

--- exercise7.rb
+++ exercise7.rb
@@ -1,4 +1,4 @@
-def display_classes (group)
+def display_classes(group)
   group.each do |cohort, number|
     puts "#{cohort}: #{number} students"
   end
@@ -12,14 +12,31 @@ students = {
 
 display_classes(students)
 
-students[:cohort4] = "43"
+students[:cohort4] = 43
 
 puts students.keys
 
-students.each do |cohort, number|
-  puts "#{number * 1.5}"
+students.each do |cohort_name, number_of_students|
+  students[cohort_name] = number_of_students * 1.05
 end
+
+display_classes(students)
 
 students.delete(:cohort2)
 
 display_classes(students)
+
+# Normal way
+total = 0
+students.values.each do |number_of_students|
+  total += number_of_students
+end
+
+# Long way using inject
+total = students.values.inject(0) {|sum, value| sum += value }
+
+# Short way with inject
+total = students.values.inject(:+)
+
+
+puts "Total number of students #{total}"
