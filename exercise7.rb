bfc57669c22fc52c0eb40b638b8ef59bbc6ed394
Vicky L <vicky.sy.loo@gmail.com>
2016-03-23 21:23:29 -0400

    added notes to exercise 7

--- exercise7.rb
+++ exercise7.rb
@@ -4,28 +4,35 @@ def display_classes(group)
   end
 end
 
+#1 create hash
 students = {
   :cohort1 => 34,
   :cohort2 => 42,
   :cohort3 => 22
 }
 
+#2 method that displays name and number of students for each cohort
 display_classes(students)
 
+#3 add cohort 4
 students[:cohort4] = 43
 
+#4 keys method to output all cohort names
 puts students.keys
 
+#5 expanded cohort size 5% for each class
 students.each do |cohort_name, number_of_students|
   students[cohort_name] = number_of_students * 1.05
 end
 
 display_classes(students)
 
+#6 delete second cohort and redisplay hash
 students.delete(:cohort2)
 
 display_classes(students)
 
+#7 bonus - total number of students across all cohorts
 # Normal way
 total = 0
 students.values.each do |number_of_students|
